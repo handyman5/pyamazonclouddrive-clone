bb4d656ea20dec9230bbd66d64c3970a81d7ba5a
sakurai.youhei@gmail.com <sakurai.youhei@gmail.com@02e808f9-35be-95c6-5397-6d64a460dd44>
2011-04-18 06:46:14 +0000

    git-svn-id: http://pyamazonclouddrive.googlecode.com/svn/trunk@9 02e808f9-35be-95c6-5397-6d64a460dd44

--- pyv8/__init__.py
+++ pyv8/__init__.py
@@ -1,13 +1,23 @@
+import os
+import sys
 import platform
 
+
 if 'Windows'==platform.system():
-  from windows.PyV8 import *
+  sys.path.insert(0,os.path.dirname(__file__)+os.sep+"windows")
 elif 'Linux'==platform.system():
   import commands
   if '64'==commands.getoutput('getconf LONG_BIT'):
-    from linux_x64.PyV8 import *
+    sys.path.insert(0,os.path.dirname(__file__)+os.sep+"linux_x64")
   else:
-    from linux_x86.PyV8 import *
+    sys.path.insert(0,os.path.dirname(__file__)+os.sep+"linux_x86")
 else:
   raise ImportError('Unsupported platform')
+
+try:
+  from PyV8 import *
+except AttributeError,e:
+  pass
+
+sys.path.pop(0)
 
