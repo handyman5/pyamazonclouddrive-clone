b5aeb7ce8edb6931bc749f444b27b457dd277949
sakurai.youhei@gmail.com <sakurai.youhei@gmail.com@02e808f9-35be-95c6-5397-6d64a460dd44>
2011-04-18 15:58:10 +0000

    git-svn-id: http://pyamazonclouddrive.googlecode.com/svn/trunk@15 02e808f9-35be-95c6-5397-6d64a460dd44

--- auth_trial.py
+++ auth_trial.py
@@ -1,5 +1,97 @@
+import httplib
+import re
+import sys
 import urllib2
+
+conn=httplib.HTTPSConnection("www.amazon.com")
+headers={"Cookie":"session-id-time=2082787201l; session-id=192-9309567-1619917; ubid-main=192-3822733-3278467;"}
+conn.request("GET","/crouddrive",None,headers)
+res=conn.getresponse()
+cookies = []
+for c in res.getheader("Set-Cookie").split(","):
+  cookies.append(re.sub(";.*","",c))
+print cookies
+print res.getheader('Location')
+conn.close()
+
+res=urllib2.urlopen("https://www.amazon.com/clouddrive/")
+begin='<form name="signIn"'
+end='</form>'
+html=begin + res.read().split(begin)[1].split(end)[0] +end
+res.close()
+
+#print html.split("action")[1]
+
+from HTMLParser import HTMLParser
+class Parser(HTMLParser):
+  def __init__(self):
+    HTMLParser.__init__(self)
+    self.key_value={}
+    self.action=""
+
+  def handle_starttag(self, tag, attrs):
+    d=dict(attrs)
+    if tag=="form":
+      print d
+      self.action=d.get("action","")
+    elif tag=='input':
+      if d.get('name'):
+        self.key_value[d.get('name')]=d.get('value','')
+  def handle_endtag(self, tag):
+    if tag=='input':
+      pass
+
+parser=Parser()
+parser.feed(html)
+parser.close()
+
+action=urllib2.urlparse.urlparse(parser.action)[2]
+params=parser.key_value.copy()
+
+#params["x"]=0
+#params["y"]=0
+params["create"]=0
+#params["metadata1"]=""
+
+
+from ConfigParser import SafeConfigParser
+parser=SafeConfigParser()
+parser.read("../amazon.ini")
+config=dict(parser.items("Credentials"))
+
+params["email"]=config["username"]
+params["password"]=config["password"]
+
+print params
+print action
+
+import urllib
+conn=httplib.HTTPSConnection("www.amazon.com")
+body=urllib.urlencode(params)
+headers={"Cookie":"; ".join(cookies)}
+
+conn.request("POST",action,body,headers)
+res=conn.getresponse()
+
+#print res.read()
+
+
+sys.exit(0)
+
+
+
+
+
+
+
+
 import cookielib
+
+amazon_cookies="""
+.amazon.com	TRUE	/	FALSE	2082787201	ubid-main	192-3822733-3278467
+.amazon.com	TRUE	/	FALSE	2082787201	session-id	192-9309567-1619917
+.amazon.com	TRUE	/	FALSE	2082787201	session-id-time	2082787201l
+"""
 
 
 #redirect_handler = urllib2.HTTPRedirectHandler() 
@@ -14,28 +106,52 @@ import cookielib
 #response =urllib2.urlopen("WHEREEVER") 
 #print response.read() 
 
-cj = cookielib.CookieJar()
+class MyCookiePolicy(cookielib.DefaultCookiePolicy):
+    """Cookie Policy for live.com
+
+    With python 2.6, cookie.DefaultCookiePolicy works. But with python 2.4,
+    cookie.DefaultCookiePolicy won't accept cookies whose version > 0 unless
+    rfc2965 is set to true. The domain ".login.live.com" isn't accepted also.
+    """
+    def set_ok(self, cookie, request):
+        # Note: Fixed for python 2.4
+        cookie.version = 0
+        if not cookielib.DefaultCookiePolicy.set_ok(self, cookie, request):
+            return False
+
+        return True
+
+policy = MyCookiePolicy(rfc2965 = True)
+
+#cj = cookielib.FileCookieJar("cookie.txt")
+#cj = cookielib.MozillaCookieJar(policy=policy)
+cj = cookielib.MozillaCookieJar()
+cj.load("cookie.txt",True)
+print cj
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 #urllib2.install_opener(opener)
 
 #res=opener.open("http://www.amazon.com/")
+print "*"*10
 
 end_point="https://www.amazon.com/clouddrive/"
-req=urllib2.Request(end_point,None,{'Cookie':'ubid-main=002-8989859-9917520; session-id-time=2082787201l;session-id=189-6539933-5925661;'})
+req=urllib2.Request(end_point)
+#print cj._cookies_for_request(req)
+print "*"*10
 
-res=opener.open(req)
+res=opener.open(end_point)
 #res.headers.headers.append('Set-Cookie: ubid-main=002-8989859-9917520; path=/; domain=.amazon.com; expires=Tue Jan 01 08:00:01 2036 GMT')
 #res.headers.headers.append('Set-Cookie: session-id-time=2082787201l; path=/; domain=.amazon.com; expires=Tue Jan 01 08:00:01 2036 GMT')
 #res.headers.headers.append('Set-Cookie: session-id=189-6539933-5925661; path=/; domain=.amazon.com; expires=Tue Jan 01 08:00:01 2036 GMT')
-for cookie in cj.make_cookies(res,req):
-  cj.set_cookie(cookie)
+#for cookie in cj.make_cookies(res,req):
+#  cj.set_cookie(cookie)
 
 print cj 
+#cj.save("_cookie.txt",True)
 print "*"*10
 
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+#opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 req=urllib2.Request(end_point)
-print cj._cookies_for_request(req)
 res=opener.open(req)
 print cj
 print "*"*10
@@ -91,5 +207,5 @@ import urllib
 
 res=opener.open(url,urllib.urlencode(params))
 #print res.read()
-
+print cj
 
