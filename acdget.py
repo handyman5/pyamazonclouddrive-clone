f373b0016c4c700f6a3a4909446e5fc6718cabe5
sakurai.youhei@gmail.com <sakurai.youhei@gmail.com@02e808f9-35be-95c6-5397-6d64a460dd44>
2011-06-04 15:39:09 +0000

    change name. acdupload->acdput, acddownload->acdget
    git-svn-id: http://pyamazonclouddrive.googlecode.com/svn/trunk@47 02e808f9-35be-95c6-5397-6d64a460dd44

--- acdget.py
+++ acdget.py
@@ -24,8 +24,8 @@
 # The Software shall be used for Younger than you, not Older.
 # 
 """
-administrator@Tualatin ~/svn/pyacd $ ./acdupload.py --help
-Usage: acdupload.py [Options] file1 file2 - ...('-' means STDIN)
+administrator@Tualatin ~/svn/pyacd $ ./acdget.py --help
+Usage: acdget.py [Options] file1 file2 - ...('-' means STDIN)
 
 Options:
   --version             show program's version number and exit
@@ -37,17 +37,19 @@ Options:
   -s FILE, --session=FILE
                         save or load login session to/from FILE
   -d PATH, --destination=PATH
-                        upload path [default: /]
+                        download path [default: ./]
+  -f, --force           override local file if it has same name [default:
+                        False]
   -v, --verbose         show debug infomation
   -q, --quiet           quiet mode
 
-This command uploads file(s) to your Amazon Cloud Drive. If there is same
-named file, uploading file is renamed automatically. (e.g. 'test.mp3' -> 'test
-(2).mp3')
+This command download file(s) from your Amazon Cloud Drive. If there is same
+named file, downloading is canceled automatically. (or use --force option)
 
-administrator@Tualatin ~/svn/pyacd $ ./acdupload.py -e someone@example.com -p xxxx ~/test.jpg 
+administrator@Tualatin ~/svn/pyacd $ ./acdget.py -s ~/.session README.TXT
 Logining to Amazon.com ... Done
-Uploading test.jpg to / ... Done
+Updating /home/administrator/.session ... Done
+Downloading /README.TXT to ./ ... Done
 """
 
 import sys
@@ -61,10 +63,10 @@ if os.path.exists(pyacd_lib_dir) and os.path.isdir(pyacd_lib_dir):
 
 import pyacd
 
-parser=OptionParser(epilog="This command uploads file(s) to your Amazon Cloud Drive. "+
-                            "If there is same named file, uploading file is renamed "+
-                            "automatically. (e.g. 'test.mp3' -> 'test (2).mp3')",
-                    usage="%prog [Options] file1 file2 - ...('-' means STDIN)",version="%prog 0.2.1")
+parser=OptionParser(epilog="This command download file(s) from your Amazon Cloud Drive. "+
+                            "If there is same named file, downloading is canceled "+
+                            "automatically. (or use --force option)",
+                    usage="%prog [Options] file1 file2 - ...('-' means STDIN)",version="%prog 0.2")
 
 parser.add_option("-e","--email",dest="email",action="store",default=None,
                   help="email address for Amazon.com")
@@ -72,8 +74,10 @@ parser.add_option("-p","--password",dest="password",action="store",default=None,
                   help="password for Amazon.com")
 parser.add_option("-s","--session",dest="session",action="store",default=None,
                   metavar="FILE",help="save or load login session to/from FILE")
-parser.add_option("-d","--destination",dest="path",action="store",default="/",
-                  help="upload path [default: %default]")
+parser.add_option("-d","--destination",dest="path",action="store",default="."+os.sep,
+                  help="download path [default: %default]")
+parser.add_option("-f","--force",dest="force",action="store_true",default=False,
+                  help="override local file if it has same name [default: %default]")
 parser.add_option("-v","--verbose",dest="verbose",action="store_true",default=False,
                   help="show debug infomation")
 parser.add_option("-q","--quiet",dest="quiet",action="store_true",default=False,
@@ -89,7 +93,7 @@ def main():
     sys.stderr.write("!! email and password are required !!\n")
     parser.print_help()
     sys.exit(2)
-    
+
   args=list(set(args))
   if "-" in args:
     args.remove("-")
@@ -100,13 +104,17 @@ def main():
     parser.print_help()
     sys.exit(2)
   else:
-    for file in args:
-      if not os.path.exists(file):
-        sys.stderr.write('Not found "%s"\n'%file)
-        sys.exit(2)
-      elif os.path.isdir(file):
-        sys.stderr.write('"%s" is not file\n'%file)
-        sys.exit(2)
+    pass
+    
+  # Check destination
+  path=opts.path
+  if path[-1]!=os.sep:path=path+os.sep
+  if not os.path.exists(path):
+    sys.stderr.write('"%s" does not exist\n'%path)
+    sys.exit(2)
+  elif not os.path.isdir(path):
+    sys.stderr.write('"%s" is not file\n'%path)
+    sys.exit(2)
 
   # Login to Amazon.com
   session=None
@@ -136,7 +144,7 @@ def main():
 
   if not opts.quiet:
     sys.stderr.write("Done\n")
-
+    
   if opts.session:
     if not opts.quiet:
       sys.stderr.write("Updating %s ... "%opts.session)
@@ -147,62 +155,40 @@ def main():
     if not opts.quiet:
       sys.stderr.write("Done\n")
 
-  # Check destination
-  path=opts.path
-  if path[0]!='/':path='/'+path
-  if path[-1]!='/':path=path+'/'
-  try:
-    dest = pyacd.api.get_info_by_path(path)
-    if dest.Type == pyacd.types.FILE:
-      sys.stderr.write('"%s" is file\n'%path)
-      sys.exit(2)
-  except pyacd.PyAmazonCloudDriveApiException,e:
-    sys.stderr.write('"%s"\n'%e.message)
-    sys.exit(2)
-
-
   for file in args:
+    if file[0]!='/':file='/'+file
     filename = os.path.basename(file)
-    f=open(file,"rb")
-    filedata = f.read()
-    f.close()
 
     if not opts.quiet:
-      sys.stderr.write("Uploading %s to %s ... "%(filename,path))
+      sys.stderr.write("Downloading %s to %s ... "%(file,path))
 
-    # create file
+    if os.path.exists(path+filename) and not opts.force:
+      sys.stderr.write("Aborted. ('%s' exists.)\n"%(path+filename))
+      continue
+
+    # get file
     if opts.verbose:
-      sys.stderr.write("create ")
-    fileobj = pyacd.api.create_by_path(path,filename)
+      sys.stderr.write("get ")
+    try:
+      fileobj = pyacd.api.get_info_by_path(file)
+    except pyacd.PyAmazonCloudDriveApiException,e:
+      sys.stderr.write("Aborted. ('%s')\n"%e.message)
+      continue
     if opts.verbose:
       sys.stderr.write("-> ")
 
-    # get upload_url
-    if opts.verbose:
-      sys.stderr.write("url ")
-    upload_url = pyacd.api.get_upload_url_by_id(fileobj.object_id,len(filedata))
-    if opts.verbose:
-      sys.stderr.write("-> ")
+    if fileobj.Type!= pyacd.types.FILE:
+      sys.stderr.write("Aborted. ('%s' is not file.)"%file)
+      continue
 
-    end_point=upload_url.http_request.end_point
-    parameters=upload_url.http_request.parameters
+    # download
+    data=pyacd.api.download_by_id(fileobj.object_id)
+    
 
-    storage_key=upload_url.storage_key
-    object_id=upload_url.object_id
-
-    # upload file
-    if opts.verbose:
-      sys.stderr.write("send ")
-    pyacd.api.upload(end_point,parameters,filename,filedata)
-    if opts.verbose:
-      sys.stderr.write("-> ")
-
-    # completeing file
-    if opts.verbose:
-      sys.stderr.write("finish ")
-    pyacd.api.complete_file_upload_by_id(object_id,storage_key)
-    if opts.verbose:
-      sys.stderr.write("-> ")
+    f=open(path+filename,"wb")
+    f.truncate()
+    f.write(data)
+    f.close()
 
     if not opts.quiet:
       sys.stderr.write("Done\n")
--- acdput.py
+++ acdput.py
@@ -24,8 +24,8 @@
 # The Software shall be used for Younger than you, not Older.
 # 
 """
-administrator@Tualatin ~/svn/pyacd $ ./acddownload.py --help
-Usage: acddownload.py [Options] file1 file2 - ...('-' means STDIN)
+administrator@Tualatin ~/svn/pyacd $ ./acdput.py --help
+Usage: acdput.py [Options] file1 file2 - ...('-' means STDIN)
 
 Options:
   --version             show program's version number and exit
@@ -37,19 +37,17 @@ Options:
   -s FILE, --session=FILE
                         save or load login session to/from FILE
   -d PATH, --destination=PATH
-                        download path [default: ./]
-  -f, --force           override local file if it has same name [default:
-                        False]
+                        upload path [default: /]
   -v, --verbose         show debug infomation
   -q, --quiet           quiet mode
 
-This command download file(s) from your Amazon Cloud Drive. If there is same
-named file, downloading is canceled automatically. (or use --force option)
+This command uploads file(s) to your Amazon Cloud Drive. If there is same
+named file, uploading file is renamed automatically. (e.g. 'test.mp3' -> 'test
+(2).mp3')
 
-administrator@Tualatin ~/svn/pyacd $ ./acddownload.py -s ~/.session README.TXT
+administrator@Tualatin ~/svn/pyacd $ ./acdput.py -e someone@example.com -p xxxx ~/test.jpg 
 Logining to Amazon.com ... Done
-Updating /home/administrator/.session ... Done
-Downloading /README.TXT to ./ ... Done
+Uploading test.jpg to / ... Done
 """
 
 import sys
@@ -63,10 +61,10 @@ if os.path.exists(pyacd_lib_dir) and os.path.isdir(pyacd_lib_dir):
 
 import pyacd
 
-parser=OptionParser(epilog="This command download file(s) from your Amazon Cloud Drive. "+
-                            "If there is same named file, downloading is canceled "+
-                            "automatically. (or use --force option)",
-                    usage="%prog [Options] file1 file2 - ...('-' means STDIN)",version="%prog 0.2")
+parser=OptionParser(epilog="This command uploads file(s) to your Amazon Cloud Drive. "+
+                            "If there is same named file, uploading file is renamed "+
+                            "automatically. (e.g. 'test.mp3' -> 'test (2).mp3')",
+                    usage="%prog [Options] file1 file2 - ...('-' means STDIN)",version="%prog 0.2.1")
 
 parser.add_option("-e","--email",dest="email",action="store",default=None,
                   help="email address for Amazon.com")
@@ -74,10 +72,8 @@ parser.add_option("-p","--password",dest="password",action="store",default=None,
                   help="password for Amazon.com")
 parser.add_option("-s","--session",dest="session",action="store",default=None,
                   metavar="FILE",help="save or load login session to/from FILE")
-parser.add_option("-d","--destination",dest="path",action="store",default="."+os.sep,
-                  help="download path [default: %default]")
-parser.add_option("-f","--force",dest="force",action="store_true",default=False,
-                  help="override local file if it has same name [default: %default]")
+parser.add_option("-d","--destination",dest="path",action="store",default="/",
+                  help="upload path [default: %default]")
 parser.add_option("-v","--verbose",dest="verbose",action="store_true",default=False,
                   help="show debug infomation")
 parser.add_option("-q","--quiet",dest="quiet",action="store_true",default=False,
@@ -93,7 +89,7 @@ def main():
     sys.stderr.write("!! email and password are required !!\n")
     parser.print_help()
     sys.exit(2)
-
+    
   args=list(set(args))
   if "-" in args:
     args.remove("-")
@@ -104,17 +100,13 @@ def main():
     parser.print_help()
     sys.exit(2)
   else:
-    pass
-    
-  # Check destination
-  path=opts.path
-  if path[-1]!=os.sep:path=path+os.sep
-  if not os.path.exists(path):
-    sys.stderr.write('"%s" does not exist\n'%path)
-    sys.exit(2)
-  elif not os.path.isdir(path):
-    sys.stderr.write('"%s" is not file\n'%path)
-    sys.exit(2)
+    for file in args:
+      if not os.path.exists(file):
+        sys.stderr.write('Not found "%s"\n'%file)
+        sys.exit(2)
+      elif os.path.isdir(file):
+        sys.stderr.write('"%s" is not file\n'%file)
+        sys.exit(2)
 
   # Login to Amazon.com
   session=None
@@ -144,7 +136,7 @@ def main():
 
   if not opts.quiet:
     sys.stderr.write("Done\n")
-    
+
   if opts.session:
     if not opts.quiet:
       sys.stderr.write("Updating %s ... "%opts.session)
@@ -155,40 +147,62 @@ def main():
     if not opts.quiet:
       sys.stderr.write("Done\n")
 
+  # Check destination
+  path=opts.path
+  if path[0]!='/':path='/'+path
+  if path[-1]!='/':path=path+'/'
+  try:
+    dest = pyacd.api.get_info_by_path(path)
+    if dest.Type == pyacd.types.FILE:
+      sys.stderr.write('"%s" is file\n'%path)
+      sys.exit(2)
+  except pyacd.PyAmazonCloudDriveApiException,e:
+    sys.stderr.write('"%s"\n'%e.message)
+    sys.exit(2)
+
+
   for file in args:
-    if file[0]!='/':file='/'+file
     filename = os.path.basename(file)
+    f=open(file,"rb")
+    filedata = f.read()
+    f.close()
 
     if not opts.quiet:
-      sys.stderr.write("Downloading %s to %s ... "%(file,path))
+      sys.stderr.write("Uploading %s to %s ... "%(filename,path))
 
-    if os.path.exists(path+filename) and not opts.force:
-      sys.stderr.write("Aborted. ('%s' exists.)\n"%(path+filename))
-      continue
-
-    # get file
+    # create file
     if opts.verbose:
-      sys.stderr.write("get ")
-    try:
-      fileobj = pyacd.api.get_info_by_path(file)
-    except pyacd.PyAmazonCloudDriveApiException,e:
-      sys.stderr.write("Aborted. ('%s')\n"%e.message)
-      continue
+      sys.stderr.write("create ")
+    fileobj = pyacd.api.create_by_path(path,filename)
     if opts.verbose:
       sys.stderr.write("-> ")
 
-    if fileobj.Type!= pyacd.types.FILE:
-      sys.stderr.write("Aborted. ('%s' is not file.)"%file)
-      continue
+    # get upload_url
+    if opts.verbose:
+      sys.stderr.write("url ")
+    upload_url = pyacd.api.get_upload_url_by_id(fileobj.object_id,len(filedata))
+    if opts.verbose:
+      sys.stderr.write("-> ")
 
-    # download
-    data=pyacd.api.download_by_id(fileobj.object_id)
-    
+    end_point=upload_url.http_request.end_point
+    parameters=upload_url.http_request.parameters
 
-    f=open(path+filename,"wb")
-    f.truncate()
-    f.write(data)
-    f.close()
+    storage_key=upload_url.storage_key
+    object_id=upload_url.object_id
+
+    # upload file
+    if opts.verbose:
+      sys.stderr.write("send ")
+    pyacd.api.upload(end_point,parameters,filename,filedata)
+    if opts.verbose:
+      sys.stderr.write("-> ")
+
+    # completeing file
+    if opts.verbose:
+      sys.stderr.write("finish ")
+    pyacd.api.complete_file_upload_by_id(object_id,storage_key)
+    if opts.verbose:
+      sys.stderr.write("-> ")
 
     if not opts.quiet:
       sys.stderr.write("Done\n")
